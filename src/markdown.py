a5a656eaab03594c6d13609a9a759e9cfdf77194
Jon <1628112+jonvanw@users.noreply.github.com>
2025-12-23 17:33:53 -0800

    Added markdown URL extraction

--- src/markdown.py
+++ src/markdown.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -16,3 +17,73 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if inside:
             raise Exception(f'Encountered unpaired "{delimiter}" delimiter in the following text: "{old_node.text}"')
     return nodes
+
+def split_nodes_url_helper(old_nodes, replaced_text, text, text_type, url):
+    if not url:
+        raise ValueError('url must be specified')
+    nodes = []
+    for old_node in old_nodes:
+        if old_node.url != None:
+            nodes.append(old_node)
+            continue
+        parts = old_node.text.split(replaced_text)
+        is_first_time = True
+        for part in parts:
+            if is_first_time:
+                is_first_time = False
+            else:
+                url_node = TextNode(text, text_type, url)
+                nodes.append(url_node)
+            
+            if part != "":
+                new_node = TextNode(part, old_node.text_type)
+                nodes.append(new_node)
+
+    return nodes
+
+def extract_markdown_images(text):
+    regex = r"!\[(.*?)\]\((.*?)\)"
+    matches = re.findall(regex, text)
+    return matches
+
+def extract_markdown_links(text):
+    regex = r"(?<!!)\[(.*?)\]\((.*?)\)"
+    matches = re.findall(regex, text)
+    return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        images = extract_markdown_images(old_node.text)
+        if not images:
+            new_nodes.append(old_node)
+            continue
+        current_nodes = [old_node]
+        for image in images:
+            current_nodes = \
+                split_nodes_url_helper(current_nodes,
+                    f'![{image[0]}]({image[1]})',
+                    image[0], 
+                    TextType.IMAGE,
+                    image[1])
+        new_nodes.extend(current_nodes)
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        links = extract_markdown_links(old_node.text)
+        if not links:
+            new_nodes.append(old_node)
+            continue
+        current_nodes = [old_node]
+        for link in links:
+            current_nodes = \
+                split_nodes_url_helper(current_nodes,
+                    f'[{link[0]}]({link[1]})',
+                    link[0], 
+                    TextType.LINK,
+                    link[1])
+        new_nodes.extend(current_nodes)
+    return new_nodes
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown import split_nodes_delimiter
+from markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 from textnode import TextNode, TextType
 
 class TestHTMLNode(unittest.TestCase):
@@ -50,6 +50,94 @@ class TestHTMLNode(unittest.TestCase):
         
         the_exception = cm.exception
         self.assertEqual('Encountered unpaired "_" delimiter in the following text: "The _italics in this text is not closed"', the_exception.args[0])
+
+    def test_extract_md_images_multi(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        actual = extract_markdown_images(text)
+        self.assertListEqual(expected, actual)
+
+    def test_extract_md_images_single(self):
+        text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        expected = [("image", "https://i.imgur.com/zjjcJKZ.png")]
+        actual = extract_markdown_images(text)
+        self.assertListEqual(expected, actual)
+
+    def test_extract_md_images_none(self):
+        text = "This is text with any images. [this is not an image]. (nor is this)"
+        expected = []
+        actual = extract_markdown_images(text)
+        self.assertListEqual(expected, actual)
+
+    def test_extract_md_images_not_links(self):
+        text = "This text contains [a link](https://www.google.com)"
+        expected = []
+        actual = extract_markdown_images(text)
+        self.assertListEqual(expected, actual)
+
+    def test_extract_md_link_single(self):
+        text = "This text includes [a link](https://www.amazon.com)"
+        expected = [('a link', 'https://www.amazon.com')]
+        actual = extract_markdown_links(text)
+        self.assertListEqual(expected, actual)
+
+    def test_extract_md_link_multiple(self):
+        text = "This text includes multiple links: [Japan](https://www.amazon.co.jp), [UK](https://www.amazon.co.uk)"
+        expected = [('Japan', 'https://www.amazon.co.jp'), ('UK', 'https://www.amazon.co.uk')]
+        actual = extract_markdown_links(text)
+        self.assertListEqual(expected, actual)
+
+    def test_extract_md_link_none(self):
+        text = "This text does not include any links."
+        expected = []
+        actual = extract_markdown_links(text)
+        self.assertListEqual(expected, actual)
+    
+    def test_extract_md_link_only_img(self):
+        text = "This text only includes an image: ![image](https://i.imgur.com/zjjcJKZ.png)"
+        expected = []
+        actual = extract_markdown_links(text)
+        self.assertListEqual(expected, actual)
+
+    def test_extract_md_both_link_img(self):
+        text = "This text includes [a link](https://www.amazon.com) and an image: ![image](https://i.imgur.com/zjjcJKZ.png)"
+        expected_link = [('a link', 'https://www.amazon.com')]
+        expected_img = [("image", "https://i.imgur.com/zjjcJKZ.png")]
+        actual_link = extract_markdown_links(text)
+        actual_img = extract_markdown_images(text)
+        self.assertListEqual(expected_img, actual_img)
+        self.assertListEqual(expected_link, actual_link)
+
+    def test_split_link(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        expected = [
+            TextNode("This is text with a link ", TextType.TEXT),
+            TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+            ),
+        ]
+        actual = split_nodes_link([node])
+        self.assertListEqual(expected, actual)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        expected = [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ]
+        actual = split_nodes_image([node])
+        self.assertListEqual(expected, actual)
 
 if __name__ == "__main__":
     unittest.main()    
